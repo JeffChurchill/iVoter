96760641bb1d477ecaace2400f3fa3fc7cf1a9f5
Jeff Churchill <github@hodaddy.com>
2013-08-27 16:46:29 -0700

    starting to flush out the voting part. not ready for primetime yet

--- app/views/polls/show.blade.php
+++ app/views/polls/show.blade.php
@@ -1,3 +1,5 @@
+<?php // TODO: Find a rock solid way to get the client;s IP  ?>
+
 @section('title')
 Show a Poll  | iVoter
 @stop
@@ -7,14 +9,23 @@ Show a Poll  | iVoter
 
 <div class="row">
 	<div class="span6 offset3 panel">
-	<h2>{{ $poll->topic }}</h2>
-	<ul class="list-unstyled">
-		@foreach ($answers as $answer)
-	    <li>{{ $answer->content }}</li>
-	    @endforeach
-	</ul>
+		<h2>{{ $poll->topic }}</h2>
 
-</div>
+		{{ Form::open(array('route' => 'votes.store')) }}
+		<input name="uip" type="hidden" value="<?php echo  $_SERVER['REMOTE_ADDR']; ?>">
+		<ul class="answer-list">
+			@foreach ($answers as $answer)
+			<li>{{ $answer->content }}</li>
+			@endforeach
+		</ul>
+
+		{{ Form::close() }}
+	</div>
 </div>
 
+@if($errors->any()) 
+<ul>
+	{{ implode('', $errors->all('<li class="error">:message</li>')) }}
+</ul>
+@endif
 @stop
